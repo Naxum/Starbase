583aecdb9db4be541ca871dd04e7a2cd51336464
Naxum <jake@naxite.com>
2014-01-09 13:30:27 -0500

    Prototyping transitions...

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,3 +1,12 @@
 //main javascript file!
 var dicks = "Woah!";
 console.log("Dang!" + dicks); 
+
+$(function(){
+	for(var i = 0; i < 5; i++)
+	{
+		var building = $("<div class='building'></div>");
+		building.css("transform", "translate(" + ((i * 20) + (i * 300) + 20) + "px, 0px)");
+		$("#buildings").append(building);
+	}
+});
